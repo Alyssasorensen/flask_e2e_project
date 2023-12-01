d8edc10c064f1e0dc52318dce5afbcf61b9d7d25
Alyssasorensen <-m>
2023-12-01 21:51:52 +0000

    updated files

--- app/app.py
+++ app/app.py
@@ -5,15 +5,6 @@ import os
 
 app = Flask(__name__)
 
-data = pd.DataFrame({
-    "Category": ["Reproductive Health", "Mental Well-being", "Fitness"],
-    "Information": [
-        "Information about reproductive health",
-        "Tips for mental well-being",
-        "Fitness routines",
-    ],
-})
-
 @app.route('/')
 def home():
     return render_template('base.html', data=data)
@@ -35,7 +26,7 @@ def fitness():
     return render_template('pages/fitness.html', data=data)
 
 # Define your specific values
-data = [
+first_data = [
     {"Year": 2019, "Male": 14.9, "Female": 15.6},
     {"Year": 2020, "Male": 13.2, "Female": 14.4},
     {"Year": 2021, "Male": 13.2, "Female": 13.9},
@@ -50,10 +41,15 @@ additional_data = [
     {"Year": 2022, "Male": 9.7, "Female": 15.5},
 ]
 
-df = pd.read_csv('https://raw.githubusercontent.com/Alyssasorensen/flask_e2e_project/main/data/raw_data%20_new.csv')
+# Load data from CSV file
+df = pd.read_csv('https://raw.githubusercontent.com/Alyssasorensen/flask_e2e_project/main/data/raw_data%20_new1.csv')
+
 @app.route('/data')
-def data_route():
-    return render_template('pages/data.html', data=data)
+def data():
+    # Sample data from the CSV file
+    sampled_data = df.sample(15).to_dict(orient='records')
+    print(sampled_data)  # Add this line to print the sampled_data
+    return render_template('pages/data.html', first_data=first_data, additional_data=additional_data, sampled_data=sampled_data)
 
 @app.route('/contact')
 def contact():
