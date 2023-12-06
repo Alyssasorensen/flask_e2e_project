d2015917c2eed68ea1103112f18474ab589b69b4
Alyssasorensen <-m>
2023-12-06 21:00:55 +0000

    updated files

--- app/app.py
+++ app/app.py
@@ -24,6 +24,10 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 oauth = OAuth(app)
 
+@app.route('/')
+def index():
+    return render_template('pages/index.html')
+
 @app.route('/google/')
 def google():
     CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
@@ -38,15 +42,17 @@ def google():
     )
 
     # Redirect to google_auth function
-    ###note, if running locally on a non-google shell, do not need to override redirect_uri
-    ### and can just use url_for as below
+    ## note, if running locally on a non-google shell, do not need to override redirect_uri
+    ## use url_for 
     redirect_uri = url_for('google_auth', _external=True)
     print('REDIRECT URL: ', redirect_uri)
     session['nonce'] = generate_token()
-    ##, note: if running in google shell, need to override redirect_uri 
+    ## note: if running in google shell, need to override redirect_uri 
     ## to the external web address of the shell, e.g.,
-    redirect_uri = 'https://e2ealyssa.azurewebsites.net/google/auth/'
+    redirect_uri = 'https://5000-cs-750560197970-default.cs-us-east1-vpcf.cloudshell.dev/google/auth/'
     return oauth.google.authorize_redirect(redirect_uri, nonce=session['nonce'])
+
+# redirect_uri = 'https://e2ealyssa.azurewebsites.net/google/auth/'
 
 @app.route('/google/auth/')
 def google_auth():
@@ -55,13 +61,13 @@ def google_auth():
     session['user'] = user
     update_or_create_user(user)
     print(" Google User ", user)
-    return redirect('/base.html')
+    return redirect('dashboard')
 
-@app.route('/base/')
-def index():
+@app.route('/dashboard/')
+def dashboard():
     user = session.get('user')
     if user:
-        return render_template('base.html', user=user)
+        return render_template('dashboard.html', user=user)
     else:
         return redirect('/')
 
@@ -69,10 +75,6 @@ def index():
 def logout():
     session.pop('user', None)
     return redirect('/')
-
-@app.route('/')
-def home():
-    return render_template('base.html', data=data)
 
 @app.route('/about')
 def about():
