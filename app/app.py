ab7a259a71f4ab60f373b0a0c09bd814bf1ebb9c
Alyssasorensen <-m>
2023-12-06 16:46:30 +0000

    updated files

--- app/app.py
+++ app/app.py
@@ -1,15 +1,74 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 import pandas as pd
 import csv
-import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from pandas import read_sql
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, inspect
 from flask_sqlalchemy import SQLAlchemy
+from authlib.integrations.flask_client import OAuth
+from authlib.common.security import generate_token
+from oauth.db_functions import update_or_create_user
+from flask_session import Session
+
+load_dotenv()
+
+GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
 
 app = Flask(__name__)
+
+app.secret_key = os.urandom(12)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+oauth = OAuth(app)
+
+@app.route('/google/')
+def google():
+    CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+    oauth.register(
+        name='google',
+        client_id=GOOGLE_CLIENT_ID,
+        client_secret=GOOGLE_CLIENT_SECRET,
+        server_metadata_url=CONF_URL,
+        client_kwargs={
+            'scope': 'openid email profile'
+        }
+    )
+
+    # Redirect to google_auth function
+    ###note, if running locally on a non-google shell, do not need to override redirect_uri
+    ### and can just use url_for as below
+    redirect_uri = url_for('google_auth', _external=True)
+    print('REDIRECT URL: ', redirect_uri)
+    session['nonce'] = generate_token()
+    ##, note: if running in google shell, need to override redirect_uri 
+    ## to the external web address of the shell, e.g.,
+    redirect_uri = 'https://e2ealyssa.azurewebsites.net/google/auth/'
+    return oauth.google.authorize_redirect(redirect_uri, nonce=session['nonce'])
+
+@app.route('/google/auth/')
+def google_auth():
+    token = oauth.google.authorize_access_token()
+    user = oauth.google.parse_id_token(token, nonce=session['nonce'])
+    session['user'] = user
+    update_or_create_user(user)
+    print(" Google User ", user)
+    return redirect('/base.html')
+
+@app.route('/base/')
+def index():
+    user = session.get('user')
+    if user:
+        return render_template('base.html', user=user)
+    else:
+        return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
 
 @app.route('/')
 def home():
@@ -84,5 +143,11 @@ def api_data():
 # if __name__ == '__main__':
 #     app.run(port=5001, debug=True)
 
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5000)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(
+        debug=True, 
+        port=5000
+    )
